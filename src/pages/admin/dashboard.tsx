a25aa50bd289a4ad01d8a526a339967f830221a7
Cristian Guaman <cristian.guaman@icloud.com>
2022-08-27 20:51:16 -0500

    feat: implemented logout

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,13 +1,25 @@
 import { useState } from 'react';
 import { NextPage } from 'next';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 import { IUser } from '@interfaces';
 
 const Dashboard: NextPage = () => {
   const [user, setUser] = useState<IUser>({ email: '', username: '' });
+  const router = useRouter();
   const getProfile = async () => {
     const { data } = await axios.get('/api/profile');
     setUser({ email: data.email, username: data.username });
+  };
+
+  const logout = async () => {
+    try {
+      const { data } = await axios.post('/api/auth/logout');
+      if (data === 'logout successfully') router.push('/login');
+    } catch (error) {
+      console.log(error);
+      router.push('/login');
+    }
   };
 
   return (
@@ -15,6 +27,7 @@ const Dashboard: NextPage = () => {
       <h1>Dashboard</h1>
       <pre>{JSON.stringify(user, null, 2)}</pre>
       <button onClick={getProfile}>get profile</button>
+      <button onClick={logout}>logout</button>
     </div>
   );
 };
---
+++ src/pages/api/auth/logout.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { serialize } from 'cookie';
+import { verify } from 'jsonwebtoken';
+
+export default function logoutHandler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { myTokenName } = req.cookies;
+  if (!myTokenName) return res.status(401).json({ error: 'no token' });
+  try {
+    verify(myTokenName, `${process.env.NEXT_PUBLIC_SECRET_KEY}`);
+    const serialized = serialize('myTokenName', '', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict', // or none for comunicating with external BACKEND
+      maxAge: 0,
+      path: '/',
+    });
+    res.setHeader('Set-Cookie', serialized);
+    return res.status(200).json('logout successfully');
+  } catch (error) {
+    return res.status(401).json({ error: 'invalid token' });
+  }
+}
