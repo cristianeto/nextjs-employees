74880de916cfab9284fa48108d24da534428b130
Cristian Guaman <cristian.guaman@icloud.com>
2022-08-29 23:46:32 -0500

    feat: create empleyees page

---
+++ src/pages/admin/employees.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+const EmployeesPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Employees Page</h1>
+    </div>
+  );
+};
+
+export default EmployeesPage;
