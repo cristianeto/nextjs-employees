1d69de1592eb2f56e61feb8df47391b6c01265a0
Cristian Guaman <cristian.guaman@icloud.com>
2022-09-03 23:13:10 -0500

    chore: installed and configured SWR

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import { SWRConfig } from 'swr';
+import fetcher from '../config/swr.config';
 import { store } from '../redux/store';
 import theme from 'styles/themeConfig';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </Provider>
+    <SWRConfig
+      value={{
+        fetcher,
+        refreshInterval: 3000, //
+      }}>
+      <Provider store={store}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Provider>
+    </SWRConfig>
   );
 }
 
